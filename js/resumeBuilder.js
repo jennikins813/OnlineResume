9ed196e2a4740b2e4c72f5380e1e82389af8691d
jennikins813 <jennikins813@yahoo.com>
2016-07-12 11:22:51 -0700

    Add name and role to resume header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Jen Follero");
+
+// var firstname = "Jen";
+// var age = 38;
+// console.log(age);
+
+// var awesomeThoughts = "I am Jen and I am AWESOME!";
+// console.log(awesomeThoughts);
+
+// .replace method:
+// [string].replace([old], [new]);
+
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Jennifer Follero");
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Be Awesome");
+
+$("#header").prepend(formattedName, formattedRole);
