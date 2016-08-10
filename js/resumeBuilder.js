465acc356cae67caef6e869c2b8c48604128cd29
jennikins813 <jennikins813@yahoo.com>
2016-08-09 20:53:38 -0700

    style: Update some formatting to meet project specs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -73,7 +73,7 @@ work.display = function() {
         var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
         $(".work-entry:last").append(formattedWorkLocation);
         $(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[i].description));
-    };
+    }
 };
 work.display();
 
@@ -83,7 +83,7 @@ var education = {
             "name": "JAL",
             "location": "El Segundo, CA",
             "degree": "BS",
-            "major": "Marketing",
+            "majors": "Marketing",
             "dates": "2007-2014",
             "url": "#"
         },
@@ -91,7 +91,7 @@ var education = {
             "name": "AdeNU",
             "location": "Philippines",
             "degree": "BS",
-            "major": "Psychology",
+            "majors": "Psychology",
             "dates": "1995-1999",
             "url": "http://www.adnu.edu.ph/"
         }
@@ -120,7 +120,7 @@ education.display = function() {
         var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
         var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
         var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
-        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
         var educationSchools = formattedNameLink + formattedDegree + formattedDates + formattedLocation + formattedMajor;
         $(".education-entry:last").append(educationSchools);
     };
@@ -134,7 +134,7 @@ education.display = function() {
         var formattedURLLink = formattedURL.replace("#", education.onlineCourses[i].url);
         var educationOnline = formattedTitleLink + formattedSchool + formattedDates + formattedURLLink;
         $(".education-entry:last").append(educationOnline);
-    };
+    }
 };
 
 education.display();
@@ -145,35 +145,35 @@ var projects = {
             "title": "Portfolio",
             "dates": "July 2016",
             "description": "Online portfolio to showcase projects built",
-            "images": "images/portfolio.png",
+            "images": ["images/portfolio.png"],
             "url": "https://github.com/jennikins813/myportfolio"  
         },
         {
             "title": "Frogger Arcade Game",
             "dates": "July 2016",
             "description": "Udacity project to recreate the classic arcade game, using Object Oriented JavaScript",
-            "images": "images/frogger.jpg",
+            "images": ["images/frogger.jpg"],
             "url": "https://github.com/jennikins813/ArcadeGame"
         },
         {
             "title": "Animal Trading Card",
             "dates": "June 2016",
             "description": "Udacity project to create an animal trading card using HTML and CSS",
-            "images": "images/animaltradingcard.png",
+            "images": ["images/animaltradingcard.png"],
             "url": "https://github.com/jennikins813/AnimalTradingCard"
         },
         {
             "title": "Quote Generator",
-            "dates": "May 2016",
+            "dates": "in progress",
             "description": "Class project for Hack Reactor Remote Prep Class",
-            "images": "images/hang_on_building_top.jpg",
+            "images": ["images/hang_on_building_top.jpg"],
             "url": "https://github.com/jennikins813/QuoteGenerator"
         },
         {
             "title": "Web Store Hackathon",
-            "dates": "May 2016",
+            "dates": "in progress",
             "description": "Project for Hack Reactor Remote Prep Class. Still a work in progress",
-            "images": " "
+            "images": [" "]
         }
     ]
 };
